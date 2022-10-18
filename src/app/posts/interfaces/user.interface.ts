dccb05d9d2c6aa167b0c818d7ee73bb22a52537c
simon320 <simonjuarezk2@gmail.com>
2022-10-18 10:15:39 -0300

    implements firebase

--- src/app/posts/interfaces/user.interface.ts
+++ src/app/posts/interfaces/user.interface.ts
@@ -2,29 +2,21 @@ export interface User {
     id?:       number;
     name:     string;
     username: string;
+    photo: string;
     email:    string;
+    birthday: string;
     password?: string;
     address:  Address;
-    phone:    string;
-    website:  string;
-    company:  Company;
+    admin: boolean;
+    amountPost: number;
 }
 
 export interface Address {
-    street:  string;
-    suite:   string;
     city:    string;
-    zipcode: string;
     geo:     Geo;
 }
 
 export interface Geo {
     lat: string;
     lng: string;
-}
-
-export interface Company {
-    name:        string;
-    catchPhrase: string;
-    bs:          string;
 }
